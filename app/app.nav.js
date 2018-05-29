131727f8176e4eeb0fc119e45707053e90fa228f
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2018-05-29 05:35:04 -0400

    add splash screen with drawer

--- app/app.nav.js
+++ app/app.nav.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import {DrawerNavigator} from 'react-navigation'
+import SplashScreen from './screens/splash'
 
-const RouteConfig = {}
-const AppNavigator = DrawerNavigator({}, RouteConfig)
+const Splash = {
+    screen: SplashScreen,
+    navigationOptions: {
+        header: null
+    }
+}
+const RouteConfig = {
+    initialRoute: 'Splash'
+}
+const AppNavigator = DrawerNavigator({
+    Splash: Splash
+}, RouteConfig)
 
 export default AppNavigator
